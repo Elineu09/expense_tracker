58e75bc8dbd728d3f64d59ddeff1e1eee8c49300
Elineu <elineutomas4@gmail.com>
2025-07-30 19:29:05 +0100

    Set text theme

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,25 @@ void main() {
           backgroundColor: kColorScheme.onPrimaryContainer,
           foregroundColor: kColorScheme.onPrimary,
         ),
+        cardTheme: const CardThemeData().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            fontSize: 14,
+            color: kColorScheme.onSecondaryContainer,
+          ),
+        ),
       ),
       home: const Expenses(), // Todo: Add main Widget here...
     ),
